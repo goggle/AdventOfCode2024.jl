23d5b804394ab66082018cff4d198ee55cdf9935
Alexander Seiler <seileralex@gmail.com>
2024-12-19 01:10:29 +0100

    [Day 16] Add solution

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ This Julia package contains my solutions for [Advent of Code 2024](https://adven
 | 13 | [:white_check_mark:](https://adventofcode.com/2024/day/13) | 3.423 ms | 1.15 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day13.jl) |
 | 14 | [:white_check_mark:](https://adventofcode.com/2024/day/14) | 75.842 ms | 275.06 KiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day14.jl) |
 <!-- | 15 | [:white_check_mark:](https://adventofcode.com/2024/day/15) | 2.647 ms | 1.49 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day15.jl) | -->
-<!-- | 16 | [:white_check_mark:](https://adventofcode.com/2024/day/16) | 33.972 ms | 53.35 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day16.jl) | -->
+| 16 | [:white_check_mark:](https://adventofcode.com/2024/day/16) | TBD | 105.84 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day16.jl) |
 <!-- | 17 | [:white_check_mark:](https://adventofcode.com/2024/day/17) | 50.170 ms | 2.72 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day17.jl) | -->
 <!-- | 18 | [:white_check_mark:](https://adventofcode.com/2024/day/18) | 630.330 μs | 484.14 KiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day18.jl) | -->
 <!-- | 19 | [:white_check_mark:](https://adventofcode.com/2024/day/19) | 2.559 ms | 1.79 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2024.jl/blob/main/src/day19.jl) | -->
--- src/day16.jl
+++ src/day16.jl
@@ -6,12 +6,12 @@ using DataStructures
 
 function day16(input::String = readInput(joinpath(@__DIR__, "..", "data", "day16.txt")))
     data = map(x -> x[1], reduce(vcat, permutedims.(map(x -> split(x, ""), split(input)))))
-    # part1(data)
-    # p1(data)
-    dist, prev = dijkstra(data)
+    startpos = findall(x -> x == 'S', data)[1]
+
+    dist, prev = dijkstra(data, startpos)
     endpos = findall(x -> x == 'E', data)[1]
     p1 = minimum(dist[endpos.I..., i] for i ∈ 1:4)
-    # return prev
+
     startpos = findall(x -> x == 'S', data)[1]
     endpositions = Tuple{Int,Int,Int}[]
     for i ∈ 1:4
@@ -21,26 +21,26 @@ function day16(input::String = readInput(joinpath(@__DIR__, "..", "data", "day16
     end
     p2 = part2(startpos, endpositions, prev, size(data)...)
     return [p1, p2]
-    return p1
 end
 
-function dijkstra(data)
-    startpos = findall(x -> x == 'S', data)[1]
-    # startdir = [0, 1]
-    # endpos = findall(x -> x == 'E', data)[1]
+function p2(data, dstart, dend, p1)
+    on_shortest_path = zeros(Bool, size(data)...)
+    for pos ∈ findall(x -> x ∈ ('.', 'E', 'S'), data)
+        if any(dstart[pos.I..., i] + dend[pos.I..., i] .== p1 for i ∈ 1:4)
+            on_shortest_path[pos] = true
+        end
+    end
+    return on_shortest_path |> sum
+end
+
+function dijkstra(data, startpos)
     dist = Dict{Tuple{Int,Int,Int},Int}()
     dist[startpos.I..., 2] = 0
     pq = PriorityQueue{Tuple{Int,Int,Int},Int}()
-    # pq[startpos.I..., 2] = 0
     prev = Dict{Tuple{Int,Int,Int},Vector{Tuple{Int,Int,Int}}}()
     for position ∈ findall(x -> x ∈ ('.', 'E', 'S'), data)
         for i ∈ 1:4
             v = (position.I..., i)
-            # if v != (startpos.I..., 2)
-            #     prev[v] = []
-            #     dist[v] = typemax(Int)
-            #     pq[v] = typemax(Int)
-            # end
             prev[v] = []
             dist[v] = typemax(Int)
             pq[v] = typemax(Int)
@@ -57,11 +57,10 @@ function dijkstra(data)
         for (v, cost) ∈ neighbours
             alt = dist[u] + cost
             if alt <= dist[v]
-                # prev[v] = u
-                # push!(prev[v], u)
-                if u ∉ prev[v]
-                    push!(prev[v], u)
+                if alt < dist[v]
+                    prev[v] = []
                 end
+                push!(prev[v], u)
                 dist[v] = alt
                 pq[v] = alt
             end
@@ -73,11 +72,8 @@ end
 function part2(startpos::CartesianIndex{2}, queue::Vector{Tuple{Int,Int,Int}}, prev::Dict{Tuple{Int,Int,Int},Vector{Tuple{Int,Int,Int}}}, nrows::Int, ncols::Int)
     visited = zeros(Bool, nrows, ncols)
     while !isempty(queue)
-        # println(queue)
         elem = popfirst!(queue)
         visited[elem[1:2]...] = true
-        # elem[1:2] == startpos.I && continue
-        # !haskey(prev, elem) && continue
         for p ∈ prev[elem]
             if p ∉ queue
                 push!(queue, p)

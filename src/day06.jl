c50b3fb118c3a443c4794076ea6a0127d5db7b0d
Alexander Seiler <seileralex@gmail.com>
2025-01-15 03:27:16 +0100

    [Day 6] Further optimize

--- src/day06.jl
+++ src/day06.jl
@@ -10,9 +10,9 @@ function day06(input::String = readInput(joinpath(@__DIR__, "..", "data", "day06
     return [p1, p2]
 end
 
-function part1(data)
+function part1(data::Matrix{Char})
     pos = start = findall(x -> x == '^', data)[1]
-    dir = [-1, 0]
+    dir = (-1, 0)
     positions = Set{CartesianIndex{2}}([pos])
     while true
         npos = CartesianIndex((pos.I .+ dir)...)
@@ -27,7 +27,7 @@ function part1(data)
     return length(positions), delete!(positions, start)
 end
 
-function part2(data, positions)
+function part2(data::Matrix{Char}, positions::Set{CartesianIndex{2}})
     start = findall(x -> x == '^', data)[1]
     graph = build_graph(data)
     ncircles = 0
@@ -43,9 +43,13 @@ function part2(data, positions)
         y = start[2]
         d = 1
         iscircle = false
+        pgraphused = false
         while true
-            x, y, d, cont = next(data, graph, pgraph, x, y, d)
-            if (x, y, d) ∈ seen
+            x, y, d, cont, frompgraph = next(data, graph, pgraph, x, y, d)
+            if !pgraphused && frompgraph
+                pgraphused = true
+            end
+            if pgraphused && (x, y, d) ∈ seen
                 iscircle = true
                 break
             end
@@ -61,23 +65,21 @@ function part2(data, positions)
     return ncircles
 end
 
-function walk!(positions::Set{CartesianIndex{2}}, data::Matrix{Char}, pos::CartesianIndex{2}, dir::Vector{Int})
-    while true
-        npos = CartesianIndex((pos.I .+ dir)...)
-        !checkbounds(Bool, data, npos) && return true, pos, dir
-        if data[npos] == '#'
-            dir = turn_right(dir)
-            return false, pos, dir
-        end
-        pos = npos
-        push!(positions, pos)
+function next(data::Matrix{Char}, graph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,Bool}}, pgraph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,Bool}}, x::Int, y::Int, d::Int)
+    if haskey(pgraph, (x, y, d))
+        return (pgraph[(x, y, d)]..., true)
+    elseif haskey(graph, (x, y, d))
+        return (graph[(x, y, d)]..., false)
+    else
+        out, pos = walk(data, CartesianIndex(x, y), number_to_dir(d))
+        return pos[1], pos[2], d, !out, false
     end
 end
 
-function walk(data::Matrix{Char}, pos::CartesianIndex{2}, dir::Vector{Int})
+function walk(data::Matrix{Char}, pos::CartesianIndex{2}, dir::Tuple{Int,Int})
     while true
         npos = CartesianIndex((pos.I .+ dir)...)
-        !checkbounds(Bool, data, npos) && return true, pos  # TODO: pos or npos?
+        !checkbounds(Bool, data, npos) && return true, pos
         if data[npos] == '#'
             return false, pos
         end
@@ -110,16 +112,13 @@ function build_priority_graph(graph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,B
     pgraph = Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,Bool}}()
     _add_obstacle_to_graph!(pgraph, data, obs)
 
-    for d ∈ 1:4
-        pos = CartesianIndex((obs.I .+ number_to_dir(d))...)
-        if checkbounds(Bool, data, pos)
-            confrontdir = opposite(d)
-            for (key, value) ∈ graph
-                minmaxx = minmax(key[1], value[1])
-                minmaxy = minmax(key[2], value[2])
-                if key[3] == turn_left(confrontdir) && pos[1] ∈ minmaxx[1]:minmaxx[2] && pos[2] ∈ minmaxy[1]:minmaxy[2]
-                    pgraph[key] = (pos[1], pos[2], confrontdir, true)
-                end
+    positions = [CartesianIndex((obs.I .+ number_to_dir(d))...) for d ∈ 1:4]
+    for (key, value) ∈ graph
+        minmaxx = minmax(key[1], value[1])
+        minmaxy = minmax(key[2], value[2])
+        for d ∈ 1:4
+            if key[3] == turn_right(d) && positions[d][1] ∈ minmaxx[1]:minmaxx[2] && positions[d][2] ∈ minmaxy[1]:minmaxy[2] && checkbounds(Bool, data, positions[d])
+                pgraph[key] = (positions[d][1], positions[d][2], opposite(d), true)
             end
         end
     end
@@ -127,22 +126,10 @@ function build_priority_graph(graph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,B
     return pgraph
 end
 
-function next(data::Matrix{Char}, graph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,Bool}}, pgraph::Dict{Tuple{Int,Int,Int},Tuple{Int,Int,Int,Bool}}, x::Int, y::Int, d::Int)
-    if haskey(pgraph, (x, y, d))
-        return pgraph[(x, y, d)]
-    elseif haskey(graph, (x, y, d))
-        return graph[(x, y, d)]
-    else
-        out, pos = walk(data, CartesianIndex(x, y), number_to_dir(d))
-        return pos[1], pos[2], d, !out
-    end
-end
-
-turn_right(dir::Vector{Int}) = [0 1 ; -1 0] * dir
+turn_right(dir::Tuple{Int,Int}) = [0 1 ; -1 0] * collect(dir) |> Tuple
 turn_right(n::Int) = mod1(n + 1, 4)
-turn_left(n::Int) = mod1(n - 1, 4)
 
-function dir_to_number(dir::Vector{Int})
+function dir_to_number(dir::Tuple{Int,Int})
     dir[1] == -1 && dir[2] == 0 && return 1
     dir[1] == 0 && dir[2] == 1 && return 2
     dir[1] == 1 && dir[2] == 0 && return 3
@@ -150,10 +137,10 @@ function dir_to_number(dir::Vector{Int})
 end
 
 function number_to_dir(n::Int)
-    n == 1 && return [-1, 0]
-    n == 2 && return [0, 1]
-    n == 3 && return [1, 0]
-    n == 4 && return [0, -1]
+    n == 1 && return (-1, 0)
+    n == 2 && return (0, 1)
+    n == 3 && return (1, 0)
+    n == 4 && return (0, -1)
 end
 
 function opposite(n::Int)

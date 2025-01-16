f450402bd0bd4c83ee1cd870777128d4008c468f
Alexander Seiler <seileralex@gmail.com>
2025-01-16 02:27:12 +0100

    [Day 9] Optimize

--- src/day09.jl
+++ src/day09.jl
@@ -1,56 +1,86 @@
 module Day09
 
 using AdventOfCode2024
+using StaticArrays
 
 
 function day09(input::String = readInput(joinpath(@__DIR__, "..", "data", "day09.txt")))
-    data, filelist, spacelist = parse_data(input)
-    p1 = part1!(copy(data))
-    p2 = part2!(data)
+    blocks = parse_input(input)
+    p1 = part1!(deepcopy(blocks))
+    p2 = part2!(blocks)
     return [p1, p2]
 end
 
-function parse_data(input)
-    data = Int[]
-    filelist = Tuple{Int,Int}[]
-    spacelist = Tuple{Int,Int}[]
-    id = 0
-    isfile = true
-    index = 1
-    for d ∈ rstrip(input)
-        n = parse(Int, d)
-        if isfile
-            push!(data, (id * ones(Int, n))...)
-            push!(filelist, (index, n))
-            id += 1
-        else
-            push!(data, (-1 * ones(Int, n))...)
-            push!(spacelist, (index, n))
-        end
-        isfile = !isfile
-        index += n
+function parse_input(input)
+    digits = parse.(Int, split(rstrip(input), ""))
+    if length(digits) % 2 == 1
+        push!(digits, 0)
     end
-    return data, filelist, spacelist
+
+    # start index of block, capacity, used space, original id
+    disk = Vector{MVector{4,Int}}()
+    current_id = 0
+    current_index = 1
+    i = 1
+    while i < length(digits)
+        push!(disk, (current_index, digits[i] + digits[i+1], digits[i], current_id))
+        current_index += digits[i] + digits[i+1]
+        current_id += 1
+        i += 2
+    end
+    return disk
 end
 
-function part1!(data)
-    ispace, inumber = 1, length(data)
-    while ispace < inumber
-        while ispace < length(data) && data[ispace] != -1
-            ispace += 1
+function part1!(blocks::Vector{MVector{4,Int}})
+    data = generate_disk(blocks)
+    i, j = 1, length(blocks)
+    while i < j
+        freeindex = blocks[i][1] + blocks[i][3]
+        free = blocks[i][2] - blocks[i][3]
+        maxtransfer = blocks[j][3]
+        transfer = free >= maxtransfer ? maxtransfer : free
+        data[freeindex:freeindex+transfer-1] .= blocks[j][4]
+        data[blocks[j][1]+blocks[j][3]-transfer:blocks[j][1]+blocks[j][3]-1] .= -1
+        blocks[i][3] += transfer
+        blocks[j][3] -= transfer
+        if blocks[i][2] == blocks[i][3]
+            i += 1
         end
-        while inumber > 0 && data[inumber] == -1
-            inumber -= 1
+        if blocks[j][3] == 0
+            j -= 1
         end
-        ispace > inumber && break
-        data[ispace], data[inumber] = data[inumber], data[ispace]
-        ispace += 1
-        inumber -= 1
     end
-    return checksum(data)
+    return data |> checksum
 end
 
-function checksum(data)
+function part2!(blocks::Vector{MVector{4,Int}})
+    data = generate_disk(blocks)
+    original_usage = [b[3] for b ∈ blocks]
+    for j ∈ reverse(axes(blocks, 1))
+        transfer = original_usage[j]
+        for i ∈ 1:j-1
+            free = blocks[i][2] - blocks[i][3]
+            if free >= transfer
+                data[blocks[i][1]+blocks[i][3]:blocks[i][1]+blocks[i][3]+transfer-1] .= blocks[j][4]
+                data[blocks[j][1]:blocks[j][1]+transfer-1] .= -1
+                blocks[i][3] += transfer
+                blocks[j][3] -= transfer
+                break
+            end
+        end
+    end
+    return data |> checksum
+end
+
+function generate_disk(blocks::Vector{MVector{4,Int}})
+    data = -1 * ones(Int, blocks[end][1] + blocks[end][2] - 1)
+    for block ∈ blocks
+        data[block[1]:block[1]+block[3]-1] .= block[4]
+    end
+    return data
+end
+
+function checksum(data::Vector{Int})
     s, i = 0, 1
     while i < length(data)
         if data[i] == -1
@@ -61,82 +91,6 @@ function checksum(data)
         i += 1
     end
     return s
-end
-
-# TODO: Debug: Why is this wrong?
-function part2!(data, filelist, spacelist)
-    for (ifilestart, ifilelength) ∈ reverse(filelist)
-        for (i, (ispacestart, ispacelength)) ∈ enumerate(spacelist)
-            if ispacelength >= ifilelength
-                data[ispacestart:ispacestart + ifilelength - 1] .= data[ifilestart]
-                data[ifilestart:ifilestart + ifilelength - 1] .= -1
-                spacelist[i] = (ispacestart + ifilelength, ispacelength - ifilelength)
-                break
-            end
-        end
-        println(string(data))
-    end
-    return checksum(data)
-end
-
-function part2!(data)
-    ifile = length(data)
-    while true
-        ifile, lenfile = prev_file(data, ifile)
-        ifile == 0 && return checksum(data)
-        ispace, lenspace = free_space(data, lenfile, ifile)
-        if ispace == 0
-            ifile -= 1
-            continue
-        else
-            data[ispace:ispace+lenfile-1] .= data[ifile]
-            data[ifile:ifile+lenfile-1] .= -1
-            ifile -= 1
-        end
-    end
-
-end
-
-function prev_file(data, i)
-    i <= 0 && return 0, 0
-    if data[i] == -1
-        while data[i] == -1
-            if i == 0
-                return 0, 0
-            end
-            i -= 1
-        end
-    end
-    id = data[i]
-    while i > 0 && data[i] == id
-        i -= 1
-    end
-    istart = i + 1
-    iend = istart
-    while iend <= length(data) && data[iend] == data[istart]
-        iend += 1
-    end
-    iend -= 1
-    return istart, iend - istart + 1
-end
-
-function free_space(data, len, imax)
-    free_indices = findall(x -> x == -1, data)
-    visited = similar(data, Bool)
-    visited .= false
-    for i ∈ free_indices
-        i >= imax && return 0, 0
-        visited[i] && continue
-        istart = iend = i
-        while iend <= length(data) && data[iend] == -1
-            visited[iend] = true
-            iend += 1
-        end
-        iend -= 1
-        l = iend - istart + 1
-        l >= len && return istart, l
-    end
-    return 0, 0
 end
 
 end # module
